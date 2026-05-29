278920af2308f7030f33e719ac5d3199f45a5633
Jürgen Gross <Juergen.Gross@datev.de>
2026-05-29 20:57:49 +0200

    styles

---
+++ src/main/java/com/example/kontoauszuege/AppShellConfig.java
@@ -0,0 +1,10 @@
+package com.example.kontoauszuege;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
+public class AppShellConfig implements AppShellConfigurator {
+}
