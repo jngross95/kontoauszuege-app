f51603be1cf93972b52f324befb29371e7fafb9a
Jürgen Gross <jngross95@gmail.com>
2026-06-29 19:07:36 +0200

    getstmts

--- src/main/java/com/example/kontoauszuege/view/DlgView.java
+++ src/main/java/com/example/kontoauszuege/view/DlgView.java
@@ -104,11 +104,6 @@ public class DlgView extends VerticalLayout implements DlgCallback {
 
             parent.add(dialog);
             dialog.open();
-            try {
-                ui.push();
-            } catch (Exception ignored) {
-                // push might not be available in some environments
-            }
         });
 
         try {
