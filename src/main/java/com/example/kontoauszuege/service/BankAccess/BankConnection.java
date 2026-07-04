0269621e8f32255981a75cc612e6742d7055ba74
Jürgen Gross <jngross95@gmail.com>
2026-07-04 20:04:32 +0200

    getstmts

--- src/main/java/com/example/kontoauszuege/service/BankAccess/BankConnection.java
+++ src/main/java/com/example/kontoauszuege/service/BankAccess/BankConnection.java
@@ -572,7 +572,11 @@ public class BankConnection implements AutoCloseable {
             {
                 //callback: reason=37 msg='Ohne die Beachtung des Ergebnisses der Empfängerüberprüfung könnte der Betrag auf ein Konto gelangen, dessen Inhaber nicht dem eingegebenen Empfänger entspricht. In diesem Fall besteht kein Erstattungsanspruch gegen uns; eine Haftung der beteiligten Zahlungsdienstleister ist ausgeschlossen.' retData=''
                 case HAVE_VOP_RESULT:
-                    retData.replace(0,retData.length(),currentConnection.bankPin);
+
+                    var rr = currentConnection.dlgCallback.dlg(currentConnection.name, "Empfängerüberprüfung (true=ja, false=nein)", msg, null);
+                        // Bei Ok einfach "true" zurückgeben, bei Abbrechen wird Exception geworfen
+                        retData.replace(0, retData.length(), rr);
+
                     break;
                 // Mit dem Passwort verschluesselt HBCI4Java die Passport-Datei.
                 // Wir nehmen hier der Einfachheit halber direkt die PIN. In der Praxis
